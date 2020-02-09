fb4afc31cb1a92716cdd331851e99c201ed9f4b8
Preetee <preetee123@gmail.com>
2020-02-09 14:26:46 +0530

    added operations

---
+++ js/myfirst.js
@@ -0,0 +1,23 @@
+var name = 2;
+console.log(name);
+console.log(typeof name);
+name = "tushar";
+console.log(name);
+console.log(typeof name);
+name = true;
+
+//Basic math Operations
+const num1=20;
+const num2=40;
+
+let val;
+val = num1 + num2;
+console.log(val);
+val = num2 - num1;
+console.log(val);
+val = num1 * num2;
+console.log(val);
+val = num2/num1;
+console.log(val);
+val = num2 % num1;
+console.log(Math.round(2.6))
